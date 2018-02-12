1fbb9984cb6f750eecb6e045f228e2f2c6cf1d95
em <emilyl@berkeley.edu>
2018-02-11 21:08:10 -0800

    completed

--- WaterLevel.py
+++ WaterLevel.py
@@ -3,19 +3,17 @@ def trap(A):
     water = 0
     while i < len(A) - 1:
         if A[i] < A[i - 1]:
-            if A[i] < A[i + 1]:
-                water += min(A[i - 1], A[i + 1]) - A[i]
-            else:
-                blockedIndex = -1
-                for j in range(i + 2, len(A)):
-                    if A[j] >= A[i - 1]:
-                        blockedIndex = j
-                        break
-                if blockedIndex != -1:
-                    for index in range(i, blockedIndex):
-                        water += A[i - 1] - A[index]
-                    i += blockedIndex - i
+            peak = -1
+            for j in range(i + 1, len(A)):
+                if A[j] >= A[j + 1]:
+                    peak = j
+                    break
+            if peak != -1:
+                for index in range(i, peak):
+                    water += min(A[i - 1], A[peak]) - A[index]
+                i = peak
         i += 1
+        print("index ", i, " Water level ", water)
     return water
 
 print(trap([0,1,0,2,1,0,1,3,2,1,2,1]))
---
+++ merge.py
@@ -0,0 +1,16 @@
+def merge(arr1, arr2):
+    pointer1, pointer2 = 0, 0
+    solution = []
+    while pointer1 < len(arr1) and pointer2 < len(arr2):
+        if arr1[pointer1] < arr2[pointer2]:
+            solution.append(arr1[pointer1])
+            pointer1 += 1
+        else:
+            solution.append(arr2[pointer2])
+            pointer2 += 1
+
+    if pointer1 >= len(arr1):
+        solution.append(arr2[pointer2:])
+    if pointer2 >= len(arr2):
+        solution.append(arr1[pointer1:])
+    return solutions
